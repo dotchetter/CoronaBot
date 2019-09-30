b0b849bb53f493ac43d2af790c40aae6d821a12c
dotchetter <dotchetter@protonmail.ch>
2019-09-30 12:51:06 +0200

    Made custom class which inherits from discord.Client instead of instantiating this class in the global scope.
    Implemented logging features with logging module.
    
    Added a refresh_schedule background task that updates the schedule
    every day at midnight.
    
    Added a friday_message background task that writes a friday
    message in the general channel every friday at 3.30.

--- src.py
+++ src.py
@@ -1,6 +1,9 @@
 import os
 import discord
 import random
+import asyncio
+import logging
+from datetime import datetime, time
 from dotenv import load_dotenv
 from schedule import Schedule
 from robbot import Brain
@@ -21,77 +24,86 @@ Synposis:
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
-CALURL = os.getenv('TIMEEDIT_URL')
+SCHDURL = os.getenv('TIMEEDIT_URL')
 
-client = discord.Client()
-schedule = Schedule(url = CALURL)
-schedule.adjust_event_hours(add_hours = 2)
+class RobBotCLient(discord.Client):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-@client.event
-async def on_ready():
-    '''
-    This method is called as soon as the bot is online.
-    '''
+        self.loop.create_task(self.friday_message())
+        self.loop.create_task(self.refresh_schedule())
+        self.brain = Brain(name = 'Rob', schedule_url = SCHDURL, schedule_hour_adjust = 2)
+        log_format = "%(asctime)s::%(levelname)s::%(name)s::%(message)s"
+        logging.basicConfig(level = logging.INFO, filename = 'bot.log', format = log_format)
 
-    for guild_name in client.guilds:
-        if guild_name == GUILD:
-            break
+    async def on_ready(self):
+        '''
+        This method is called as soon as the bot is online.
+        '''
+        for guild_name in client.guilds:
+            if guild_name == GUILD:
+                break
+        logging.info(f'Bot is online at {guild_name}')
 
-    print(
-        f'{client.user} says: Rusty bolts and fried circuits - I feel alive!',
-        f'{client.user} is connected to {guild_name}',
-        sep = '\n'
-    )
-
-@client.event
-async def on_member_join(member):
-    '''
-    If a new member just joined our server, greet them 
-    warmly!
-    '''
-
-    greeting_phrase = f'Bless my silicon! {member.name} - Welcome! Please nick your real name.'
-
-    await member.create_dm()
-    await member.dm_channel.send(greeting_phrase)
-
-
-@client.event
-async def on_message(message):
-    '''
-    Respond to a message in the channel if someone
-    calls on the bot by name, asking for commands.
-    '''
-
-    recursive_response = False
-    body = message.content.lower()
-
-    if message.author == client.user:
-        recursive_response = True
+    async def on_member_join(self, member):
+        '''
+        If a new member just joined our server, greet them warmly!
+        '''
+        greeting_phrase = self.brain.greet(member.name)
     
-    if 'hej rob' in body and not recursive_response:
-        if 'klassrum' in body or 'när börjar' in body or 'nästa lektion' in body:
-            classroom = schedule.next_lesson_classroom
-            time = schedule.next_lesson_time
-            date = schedule.next_lesson_date
-            response = f'Nästa lektion är i {classroom}, {date} klockan {time} :smirk:'
-        elif 'dagens schema' in body:
-            if schedule.todays_lessons:
-                lessons_string = str('\n').join(schedule.todays_lessons)
-                response = f'Här är dagens schema:\n{lessons_string}'
-            else:
-                response = 'Det ser inte ut att finnas några fler lektioner på schemat idag.'
-        else:
-            response = 'Hmm, jag är inte säker på att jag fattade det där.'
+        await member.create_dm()
+        await member.dm_channel.send(greeting_phrase)
+        logging.info(f'Bot said {greeting_phrase}')
+    
+    async def on_message(self, message):
+        '''
+        Respond to a message in the channel if someone
+        calls on the bot by name, asking for commands.
+        '''
+        now = datetime.now().strftime('%Y-%m-%d -- %H:%M:%S')
+        body = message.content.lower()
+    
+        if 'hej rob' in body and message.author != client.user:
+            response = self.brain.respond_to(body)
+            await message.channel.send(response)
+            logging.info(f'{message.author} said {message.content}')
+            logging.info(f'bot said {response}')
+    
+    async def friday_message(self):
+        '''
+        If it is friday and it's 15.30, tell everyone
+        how awesome this is in the General channel.
+        '''
+        await client.wait_until_ready()        
+        channel = self.get_channel(626383773201727518)
+        now_time = None
+        message = 'Fredag!!! Dags för fredagsölen! :beers:'
+        message_time = time(15, 30)
         
-        await message.channel.send(response)
+        while not self.is_closed():
+            await asyncio.sleep(35)
+            if self.brain.schedule.weekday == 'friday':
+                now = self.brain.schedule.current_time
+                now_time = time(now.hour, now.minute)
+                if now_time == message_time:
+                    await channel.send(message)
+                    logging.info(f'bot said {message}')
 
-        # Debug
-        print(
-            f' - devlog: human said {message.content}',
-            f' - devlog: bot said {response}',
-            sep = '\n'
-        )
+    async def refresh_schedule(self):
+        '''
+        Refresh the Schedule object with a new updated
+        variant of the schedule from the web by using
+        its own method for this. Perform this action
+        daily at midnight.
+        '''
+        await client.wait_until_ready()
+        while not self.is_closed():
+            await asyncio.sleep(3600)
+            if self.brain.schedule.current_time.hour == 0:
+                self.brain.schedule.set_calendar()
+                logging.info(f'Refreshed calendar object')
+
 
 if __name__ == '__main__':
+    client = RobBotCLient()
     client.run(TOKEN)
