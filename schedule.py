85d73ceaeb1ad75ea79e479f7ab29bc580837f85
dotchetter <dotchetter@protonmail.ch>
2019-10-01 16:19:00 +0200

    Improved time delta logic for handling hour increment for every event

--- schedule.py
+++ schedule.py
@@ -5,6 +5,7 @@ from custom_errs import *
 from datetime import datetime, timedelta, time
 from urllib.request import urlopen
 from enum import Enum
+from sys import platform
 
 
 '''
@@ -24,146 +25,158 @@ Synposis:
 '''
 
 class Schedule:
-	'''
-	Parse an .ics url and fetch the data for this calendar.
-	The data will be used to return classroom for the day,
-	the day after and similar requests in a simple format 
-	with properties.
-	'''
-	def __init__(self, url = str):
-		self._url = url
-		self.set_calendar()
+    '''
+    Parse an .ics url and fetch the data for this calendar.
+    The data will be used to return classroom for the day,
+    the day after and similar requests in a simple format 
+    with properties.
+    '''
+    def __init__(self, url = str):
+        self._url = url
+        self.set_calendar()
+        self.truncate_event_name()
 
-	def adjust_event_hours(self, add_hours = int):
-		'''
-		Adjust the hour in a calendar event by (n) hours. 
-		Expect a datetime.time instance in event parameter.
-		This will add a .hour and a .minute property to the 
-		Calendar() objects passed to this method.
-		'''
-		for event in self.schedule:
-			try:
-				hour = event.begin.hour
-				minute = event.begin.minute
-				year = self.current_time.year
-				month = self.current_time.month
-				day = self.current_time.day
-	
-				event_datetime = datetime(year, month, day, hour, minute)
-				event_datetime += timedelta(hours = add_hours)
-				event.begin.hour = event_datetime.hour
-				event.begin.minute = event_datetime.minute
-			except ValueError as e:
-				msg = f'Could not adjust {event} with {add_hours} hrs: {e}'
-				raise TimezoneAdjustmentError(msg)
-		return True
+    def adjust_event_hours(self, hourdelta = int):
+        '''
+        Adjust the hour in a calendar event by (n) hours.
+        Create a time() property for both the event begin and 
+        event end, with corrected hour stamps according to hourdelta
+        parameter. The instance attribute will be accessible through
+        self.begin.time and self.end.time.
+        '''
+        for event in self.schedule:
+            try:
+                year = self.current_time.year
+                month = self.current_time.month
+                day = self.current_time.day
+                
+                begin_hour, begin_minute = event.begin.hour, event.begin.minute
+                end_hour, end_minute = event.end.hour, event.end.minute
 
-	def set_calendar(self):
-		'''
-		Get data from the timeedit servers containing the
-		schedule for class IoT19 2 weeks ahead. This callable
-		will refresh the .ics Calendar object.
-		'''
-		try:
-			calendar = ics.Calendar(urlopen(self._url).read().decode())
-		except ValueError:
-			msg = 'Could not parse calendar url, verify server status and access.'
-			raise InvalidCalendarUrl(msg)
-		self._calendar = calendar
+                begin_time = datetime(year, month, day, begin_hour, begin_minute)
+                end_time = datetime(year, month, day, end_hour, end_minute)
+                
+                begin_time += timedelta(hours = hourdelta)
+                end_time += timedelta(hours = hourdelta)
 
-	@property
-	def today(self):
-		return datetime.now().date()
+                event.begin.adjusted_time = time(begin_time.hour, begin_time.minute)
+                event.end.adjusted_time = time(end_time.hour, end_time.minute)
 
-	@property
-	def weekday(self):
-		weekdays = {
-			1:'monday',
-			2:'tueday',
-			3:'wednesday',
-			4:'thursday',
-			5:'friday',
-			6:'saturday',
-			7:'sunday'
-		}
-		return weekdays[self.today.isoweekday()]
+            except ValueError as e:
+                msg = f'Could not adjust {event} with {hourdelta} hrs: {e}'
+                raise TimezoneAdjustmentError(msg)
+        return True
 
-	@property
-	def current_time(self):
-		return datetime.now()
+    def set_calendar(self):
+        '''
+        Get data from the timeedit servers containing the
+        schedule for class IoT19 2 weeks ahead. This callable
+        will refresh the .ics Calendar object.
+        '''
+        try:
+            calendar = ics.Calendar(urlopen(self._url).read().decode())
+        except ValueError:
+            msg = 'Could not parse calendar url, verify server status and access.'
+            raise InvalidCalendarUrl(msg)
+        self._calendar = calendar
 
-	@property
-	def schedule(self):
-		_schedule = list(self._calendar.events)
-		_schedule.sort()
-		return _schedule
+    def truncate_event_name(self):
+        '''
+        Truncate sensitive name data in events, containing the
+        name of the teacher holding the class. This will reduce
+        the privacy issue of storing names in log files.
+        '''
 
-	@property
-	def todays_events(self):
-		return [i for i in self.schedule if i.begin.date() == self.today]
-	
-	@property
-	def todays_lessons(self):
-		'''
-		Iterate through the lessons of today, return these in a friendly
-		string with properties such as start and end time with locations.
-		'''
-		friendly_output = []
+        for event in self.schedule:
+            event.name = f"{event.name.split(',')[0]},{event.name.split(',')[-1]}"
 
-		lessons = self.todays_events  
-		if len(lessons):
-			for lesson in lessons:
-				if lesson.begin.minute < 10:
-					minute = f'0{lesson.begin.minute}'
-				else:
-					minute = lesson.begin.minute	
-				hour = lesson.begin.hour
-				name = lesson.name.split(',')[-1].strip()
-				friendly_time = f'{hour}:{minute}'
-				location = lesson.location
-				friendly_string = f'{name} klockan {friendly_time} i {location}'
-				friendly_output.append(friendly_string)
-			return friendly_output
-		return None
+    @property
+    def today(self):
+        return datetime.now().date()
 
-	@property
-	def next_lesson(self):
-		'''
-		Evaluate what lesson is the next on schedule. Iterate
-		through the list of lessons for today. Compare the current
-		time with each lesson start time, return the upcoming one.
-		Adjust for calendar timezone with 2hrs. If no lesson is found
-		for today, iterate over the entire sorted schedule and return
-		the first lesson that lies in the future.
-		'''
-		lesson = None
+    @property
+    def weekday(self):
+        weekdays = {
+            1:'monday',
+            2:'tueday',
+            3:'wednesday',
+            4:'thursday',
+            5:'friday',
+            6:'saturday',
+            7:'sunday'
+        }
+        return weekdays[self.today.isoweekday()]
 
-		if self.todays_events:
-			for event in self.todays_events:
-				if self.current_time.hour < event.begin.hour:
-					lesson = event
-					break
-		if not lesson:
-			for event in self.schedule:
-				if event.begin.date() > self.today:
-					lesson = event
-					break
-		
-		return lesson
+    @property
+    def current_time(self):
+        return datetime.now()
 
-	@property
-	def next_lesson_classroom(self):
-		return self.next_lesson.location
+    @property
+    def schedule(self):
+        _schedule = list(self._calendar.events)
+        _schedule.sort()
+        return _schedule
 
-	@property
-	def next_lesson_name(self):
-		return self.next_lesson.name
-	
-	@property
-	def next_lesson_time(self):
-		return f'{self.next_lesson.begin.hour}'
+    @property
+    def todays_events(self):
+        return [i for i in self.schedule if i.begin.date() == self.today]
+    
+    @property
+    def todays_lessons(self):
+        '''
+        Iterate through the lessons of today, return these in a friendly
+        string with properties such as start and end time with locations.
+        '''
+        output = []
 
-	@property
-	def next_lesson_date(self):
-		return f'{self.next_lesson.begin.date()}'
-	
+        if len(self.todays_events):
+            for event in self.todays_events:
+                name = event.name.split(',')[-1].strip()
+                event_start = event.begin.adjusted_time.strftime('%H:%M')
+                event_end = event.end.adjusted_time.strftime('%H:%M')
+                output.append(f'{name}, {event_start} - {event_end} i {event.location}')
+            if platform == 'win32':
+                return output
+            else:
+                return '\n'.join(output)
+        return None
+
+    @property
+    def next_lesson(self):
+        '''
+        Evaluate what lesson is the next on schedule. Iterate
+        through the list of lessons for today. Compare the current
+        time with each lesson start time, return the upcoming one.
+        Adjust for calendar timezone with 2hrs. If no lesson is found
+        for today, iterate over the entire sorted schedule and return
+        the first lesson that lies in the future.
+        '''
+        lesson = None
+
+        if self.todays_events:
+            for event in self.todays_events:
+                if self.current_time.hour < event.begin.adjusted_time.hour:
+                    lesson = event
+                    break
+        if not lesson:
+            for event in self.schedule:
+                if event.begin.date() > self.today:
+                    lesson = event
+                    break
+        return lesson
+
+    @property
+    def next_lesson_classroom(self):
+        return self.next_lesson.location
+
+    @property
+    def next_lesson_name(self):
+        return self.next_lesson.name
+    
+    @property
+    def next_lesson_time(self):
+        return f'{self.next_lesson.begin.adjusted_time.strftime("%H:%M")}'
+
+    @property
+    def next_lesson_date(self):
+        return f'{self.next_lesson.begin.date()}'
