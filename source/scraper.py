53e6e58c46de0cadab244b6b758a3395c9b58e18
dotchetter <dotchetter@protonmail.ch>
2019-11-25 17:25:51 +0100

    New module: Scraper.
    The Scraper class in the scraper module file, is used as a way
    to scrape the lunch restaurant website at campus. The logic in this
    object is restricted to this site only due to various methods of
    extracting data.
    
    The object returns a Menu() instance which holds a structured
    set of data with mappings to each weekday, mapped by number of weekday.

---
+++ source/scraper.py
@@ -0,0 +1,71 @@
+from datetime import datetime
+from bs4 import BeautifulSoup
+from urllib import request
+from custom_errs import ScrapingError
+from menu import Menu
+'''
+Details:
+    2019-11-24
+
+Module details:
+	Lunch menu scraping feature
+
+Synposis:
+	Provide a way for the chatbot to scrape
+	the lunch menu for the restaurant in school.
+	Return the menu scraped off the website.
+'''
+
+class Scraper:
+	'''
+	Scrape the website for the lunch restaurant.
+	Return dict with comprehended text from a 
+	parsed website. Look for defined tag using
+	bs4, and ignore tags that are not of interest
+	when looking for the lunch menu.
+	'''
+
+	def __init__(self, url = None):
+		self.url = url
+
+	@property
+	def url(self):
+		return self._url
+
+	@url.setter
+	def url(self, value):
+		self._url = value
+
+	@property
+	def response(self):
+		try:
+			r = request.urlopen(self.url)
+		except Exception as e:
+			return None
+		return r.read() if r.status == 200 else None	
+
+	@property
+	def soup(self):
+		if self.response is not None:
+			return BeautifulSoup(self.response, 'html.parser') 
+
+	def get(self):
+		'''
+		Scrape the website for menu text. Returns Menu
+		instance.
+		'''
+		if self.soup is not None:
+			menu = self.soup.find_all('strong')
+		else:
+			return ScrapingError('Invalid response')
+		
+		for index, tag in enumerate(menu):
+			if 'måndag' in tag.text.lower():
+				startsat = index + 1
+			elif 'kontakta' in tag.text.lower():
+				endsat = index
+		return Menu(menu[startsat:endsat])
+
+if __name__ == '__main__':
+	s = Scraper(url = 'http://restauranghjulet.se/lunch-meny/')
+	print(s.get())
