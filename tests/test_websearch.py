aaa4156a0ed2c099dae41f65092f0b494184edc6
dotchetter <dotchetter@protonmail.ch>
2019-12-17 17:24:26 +0100

    tests added for the websearch module

--- tests/test_websearch.py
+++ tests/test_websearch.py
@@ -1,27 +1,25 @@
+import unittest
+from dotenv import load_dotenv
 from source.custom_errs import AccessViolation
 from source.websearch import Websearch
 from source.client import  load_environment
-from dotenv import load_dotenv
-import unittest
 
 environment_vars = load_environment()
 
 class test_websearch(unittest.TestCase):
+
+    mock_obj = Websearch(developerKey = environment_vars['GOOGLE_API_KEY'],
+                            customsearch_id = environment_vars['GOOGLE_CSE_ID'])
     
     def test_instantiation(self):
-        mock_obj = Websearch(developerKey = environment_vars['GOOGLE_API_KEY'],
-                            customsearch_id = environment_vars['GOOGLE_CSE_ID'])
-        self.assertIsInstance(mock_obj, Websearch)
+        self.assertIsInstance(test_websearch.mock_obj, Websearch)
 
     @unittest.expectedFailure
     def test_private_developer_key(self):
-        mock_obj = Websearch(developerKey = environment_vars['GOOGLE_API_KEY'],
-                            customsearch_id = environment_vars['GOOGLE_CSE_ID'])
-        self.assertRaises(custom_errs.AccessViolation, mock_obj.developerKey)
+        self.assertRaises(custom_errs.AccessViolation, 
+            test_websearch.mock_obj.developerKey)
 
 
     def test_search_feature(self):
-        mock_obj = Websearch(developerKey = environment_vars['GOOGLE_API_KEY'],
-                            customsearch_id = environment_vars['GOOGLE_CSE_ID'])
-        res = mock_obj.search('google')
+        res = test_websearch.mock_obj.search('google')
         self.assertIsNotNone(res)
