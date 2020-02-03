ba01a691f73ea32b5a69326b7d87db033b6f9f59
dotchetter <dotchetter@protonmail.ch>
2020-02-03 22:56:54 +0100

    Separate file for the enum classes for easier editing

---
+++ source/commandintegrator/enumerators.py
@@ -0,0 +1,50 @@
+from enum import Enum, auto
+
+class CommandSubcategory(Enum):
+    '''
+    Constant enumerators for matching keywords against when
+    deciding which response to give a given command.
+    '''
+    SCHEDULE_NEXT_LESSON = auto()
+    SCHEDULE_TODAYS_LESSONS = auto()
+    SCHEDULE_TOMORROWS_LESSONS = auto()
+    SCHEDULE_CURRICULUM = auto()
+    REMINDER_REMEMBER_EVENT = auto()
+    REMINDER_SHOW_EVENTS = auto()
+    ADJECTIVE = auto()
+    TELL_JOKE = auto()
+    TIMENOW = auto()
+    WEBSEARCH = auto()
+    LUNCH_TODAY = auto()
+    LUNCH_YESTERDAY = auto()
+    LUNCH_TOMORROW = auto()
+    LUNCH_DAY_AFTER_TOMORROW = auto()
+    LUNCH_FOR_WEEK = auto()
+    SHOW_BOT_COMMANDS = auto()
+    UNIDENTIFIED = auto()
+
+class CommandCategory(Enum):
+    '''
+    Various commands that are supported. 
+    '''
+    LUNCH_MENU = auto()
+    TELL_JOKE = auto()
+    SCHEDULE = auto()
+    REMINDER = auto()
+    WEB_SEARCH = auto()
+    PERSONAL = auto()
+    UNIDENTIFIED = auto()
+
+class CommandPronoun(Enum):
+    '''
+    Identifiable pronouns in recieved commands.
+    __lt__ is implemented in order for a sorted
+    set of instances to be returned.
+    '''
+    INTERROGATIVE = auto()
+    PERSONAL = auto()
+    POSSESSIVE = auto()
+    UNIDENTIFIED = auto()
+
+    def __lt__(self, other):
+        return self.value < other.value
