73ec3ebb4005e67b437fe4641e6153bf8b8482ac
dotchetter <dotchetter@protonmail.ch>
2020-03-10 14:47:30 +0100

    Scheduling implemented of automatic messages by the usage of a custom version of the schedule module. An instance of the schedule.Scheduler() class is used by the robbottherobot class and is iterated over indefinitely asynchronously.
    Added docstring to the Load environment method

--- source/client.py
+++ source/client.py
@@ -34,8 +34,9 @@ class RobBotClient(discord.Client):
     
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        #self.loop.create_task(self.run_scheduler())
+        self.loop.create_task(self.run_scheduler(651080743388446750))
         self._guild = kwargs['DISCORD_GUILD']
+        self._scheduler = schedule.Scheduler()
 
     @logger
     async def on_ready(self):
@@ -46,7 +47,7 @@ class RobBotClient(discord.Client):
             if guild_name == self._guild:
                 break
     @logger
-    async def on_member_join(self, member):
+    async def on_member_join(self, member: discord.Member):
         '''
         If a new member just joined our server, greet them warmly!
         '''
@@ -55,7 +56,7 @@ class RobBotClient(discord.Client):
         await member.dm_channel.send(greeting_phrase)
     
     @logger    
-    async def on_message(self, message):
+    async def on_message(self, message: discord.Message):
         '''
         Respond to a message in the channel if someone
         calls on the bot by name, asking for commands.
@@ -64,22 +65,43 @@ class RobBotClient(discord.Client):
         if message.content.lower().startswith('rob') and message.author != client.user:
             response = processor.process(message).response()
             await message.channel.send(response)
+
     @logger            
-    async def run_scheduler(self):
+    async def run_scheduler(self, channel: int):
         '''
         Loop indefinitely and send messages that are pre-
         defined on a certain day and a certain time. 
         '''
 
         await client.wait_until_ready()
-        channel = self.get_channel(651080743388446750) # Private. Edit on server.
+        channel = self.get_channel(channel) # Private. Edit on server.
         
         while not self.is_closed():            
-            result = schedule.run_pending()
-            if result: channel.send(result)
+            result = self.scheduler.run_pending(passthrough = True)
+            if result: 
+                for _, value in result.items():
+                    if not value: 
+                        continue
+                    else: 
+                        await channel.send(value)
+            await asyncio.sleep(0.1)
+                        
+    @property
+    def scheduler(self):
+        return self._scheduler
+   
 
-def load_environment(env_var_strings: list):
-    
+def load_environment(env_var_strings: list) -> dict:
+    """
+    Return a dictionary comprehended with values for
+    environment variables used to authenticate various
+    interfaces throughout the bot application, loaded
+    from an .env file by the use of load_dotenv().
+
+    :param env_var_strings: 
+        list with all keys to populate with value, the 
+        same as in the ones in the .env file
+    """
     load_dotenv()
     var_dict = {}
 
@@ -105,36 +127,50 @@ if __name__ == '__main__':
         'CORONA_API_RAPIDAPI_KEY'
     ]
 
+    commandintegrator_settings_file = 'C:\\users\\admin\\git\\robbottherobot\\source\\commandintegrator\\commandintegrator.settings.json'
+    corona_translation_file = 'C:\\users\\admin\\git\\robbottherobot\\source\\country_eng_swe_translations.json'
 
-    with open('C:\\users\\si\\git\\robbottherobot\\source\\commandintegrator\\commandintegrator.settings.json', 'r', encoding = 'utf-8') as f:
+    with open(commandintegrator_settings_file, 'r', encoding = 'utf-8') as f:
         default_responses = json.loads(f.read())['default_responses']
 
     environment_vars = load_environment(enviromnent_strings)
     
     processor = CommandProcessor(
         pronoun_lookup_table = PronounLookupTable(), 
-        default_responses = default_responses
-    )
+        default_responses = default_responses)
     
+
+
+    #  --- Instantiate the feature objects used and the discord client object ---
+
     lunchmenu_ft = LunchMenuFeature(url = environment_vars['LUNCH_MENU_URL'])
     schedule_ft = ScheduleFeature(url = environment_vars['TIMEEDIT_URL'])
     corona_ft = CoronaSpreadFeature(
                     CORONA_API_URI = environment_vars['CORONA_API_URI'],
                     CORONA_API_RAPIDAPI_HOST = environment_vars['CORONA_API_RAPIDAPI_HOST'],
                     CORONA_API_RAPIDAPI_KEY = environment_vars['CORONA_API_RAPIDAPI_KEY'],
-                    translation_file_path = 'C:\\users\\si\\git\\robbottherobot\\source\\country_eng_swe_translations.json'
-                )
+                    translation_file_path = corona_translation_file)
 
     redditjoke_ft = RedditJokeFeature(
                         client_id = environment_vars['REDDIT_CLIENT_ID'], 
                         client_secret = environment_vars['REDDIT_CLIENT_SECRET'],
-                        user_agent = environment_vars['REDDIT_USER_AGENT']
-                    )
+                        user_agent = environment_vars['REDDIT_USER_AGENT'])
     
     processor.features = (lunchmenu_ft, lunchmenu_ft, corona_ft, redditjoke_ft)    
-    
-    schedule.every().day.at('08:00').do(schedule_ft.get_todays_lessons, scheduled_call = True)
-    schedule.every(5).seconds.do(schedule_ft.get_todays_lessons, scheduled_call = True)
-
     client = RobBotClient(**environment_vars)
+    
+    
+
+    """
+ 
+    Add scheduled methods here. If your method needs parameters, 
+    simply add them after the name of the method. here's an example:
+    
+    <<< client.scheduler.every(1).minute.do(add_integers, a = 10, b = 5) >>>
+
+    """
+
+    client.scheduler.every().day.at('08:00').do(schedule_ft.get_todays_lessons)
+    client.scheduler.every(10).seconds.do(schedule_ft.get_todays_lessons)
+
     client.run(environment_vars['DISCORD_TOKEN'])
