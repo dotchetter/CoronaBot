5adee6fdc918296321a8e86e4004d385ecb3c043
dotchetter <dotchetter@protonmail.ch>
2020-03-15 21:57:50 +0100

    Removed features not used in this branch of the RobBotTheRobot project

--- source/commandintegrator/enumerators.py
+++ source/commandintegrator/enumerators.py
@@ -5,22 +5,6 @@ class CommandSubcategory(Enum):
     Constant enumerators for matching keywords against when
     deciding which response to give a given command.
     """
-    SCHEDULE_NEXT_LESSON = auto()
-    SCHEDULE_TODAYS_LESSONS = auto()
-    SCHEDULE_TOMORROWS_LESSONS = auto()
-    SCHEDULE_CURRICULUM = auto()
-    
-    REMINDER_REMEMBER_EVENT = auto()
-    REMINDER_SHOW_EVENTS = auto()
-    
-    TELL_JOKE = auto()
-
-    LUNCH_TODAY = auto()
-    LUNCH_YESTERDAY = auto()
-    LUNCH_TOMORROW = auto()
-    LUNCH_DAY_AFTER_TOMORROW = auto()
-    LUNCH_FOR_WEEK = auto()
-
     CORONA_SPREAD_TOTAL_DEATHS = auto()
     CORONA_SPREAD_TOTAL_RECOVERIES = auto()
     CORONA_SPREAD_TOTAL_INFECTIONS = auto()
@@ -35,25 +19,14 @@ class CommandSubcategory(Enum):
     CORONA_RECOVERIES_BY_QUERY = auto()
     CORONA_DATA_TIMESTAMP = auto()
     CORONA_NEW_CASES_BY_QUERY = auto()
-
-    RANKING_DOWN = auto()
-    RANKING_UP = auto()
-    RANKING_FOR_MEMBER = auto()
-    RANKING_FOR_ALL = auto()
-
     UNIDENTIFIED = auto()
 
 class CommandCategory(Enum):
     """
     Various commands that are supported. 
     """
-    LUNCH_MENU = auto()
-    TELL_JOKE = auto()
-    SCHEDULE = auto()
-    REMINDER = auto()
-    PERSONAL = auto()
+
     CORONA_SPREAD = auto()
-    RANKING = auto()
     UNIDENTIFIED = auto()
 
 class CommandPronoun(Enum):
