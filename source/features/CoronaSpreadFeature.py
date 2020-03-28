860c833e1e34992a15168f9dc3a27f5d693feb01
dotchetter <dotchetter@protonmail.ch>
2020-03-28 14:10:34 +0100

    Reduction of code after the new update with CommandIntegrator, rendering the need for subcategories and enum bindings moot. Added a few keywords to match certain queries after seeing users asking reasonable questions that the bot can't answer.

--- source/features/CoronaSpreadFeature.py
+++ source/features/CoronaSpreadFeature.py
@@ -2,7 +2,7 @@ import discord
 import commandintegrator as fw
 import fake_useragent
 import coronafeatureclient as coronafeatureclient
-from commandintegrator.enumerators import CommandPronoun, CommandCategory, CommandSubcategory
+from commandintegrator.enumerators import CommandPronoun
 from commandintegrator.logger import logger
 
 
@@ -15,76 +15,58 @@ class CoronaSpreadFeature(fw.FeatureBase):
 
     FEATURE_KEYWORDS = (
         'land',
+        'totalt',
         'många',
         'corona',
         'coronafall'
     )
 
-    data_timestamp_1 = {'när': ('uppdaterad', 'uppdaterades', 'statistik', 'statistiken')}
-    data_timestamp_2 = {'hur': ('gammal', 'data', 'datan')}
-
-    total_deaths = {'totalt': ('dött', 'omkommit', 'döda')}
-    total_recoveries = {'totalt': ('friska', 'tillfrisknat', 'återhämtat')}
-    total_cases = {'totalt': ('smittade', 'smittats', 'sjuka')}
-
-    most_deaths = {'flest': ('döda', 'dödsfall', 'omkommit', 'omkomna', 'dött')}
-    most_recoveries = {'flest': ('friska', 'tillfrisknat')}
-    most_cases = {'flest': ('smittade', 'sjuka')}
-
-    least_deaths = {'minst': ('döda', 'dödsfall', 'omkomna', 'omkommit', 'dött')}
-    least_recoveries = {'minst': ('smittade', 'sjuka')}
-    least_cases = {'minst': ('friska', 'tillfrisknat')}
-
-    infections_by_query_1 = {'har': ('smittats', 'sjuka')}
-    infections_by_query_2 = {'är': ('smittade', 'sjuka')}
-    deaths_by_query = {'har': ('dött', 'omkommit')}
-    recoveries_by_query = {'har': ('friska', 'tillfrisknat')}
-    new_cases_by_query = {'hur': ('nya', 'nytt', 'fall')}    
-
-
-    FEATURE_SUBCATEGORIES = {
-        str(data_timestamp_1): CommandSubcategory.CORONA_DATA_TIMESTAMP,
-        str(data_timestamp_2): CommandSubcategory.CORONA_DATA_TIMESTAMP,
-        str(total_deaths): CommandSubcategory.CORONA_SPREAD_TOTAL_DEATHS,
-        str(total_recoveries): CommandSubcategory.CORONA_SPREAD_TOTAL_RECOVERIES,
-        str(total_cases): CommandSubcategory.CORONA_SPREAD_TOTAL_INFECTIONS,
-        str(most_deaths): CommandSubcategory.CORONA_SPREAD_MOST_DEATHS,
-        str(most_recoveries): CommandSubcategory.CORONA_SPREAD_MOST_RECOVERIES,
-        str(most_cases): CommandSubcategory.CORONA_SPREAD_MOST_INFECTIONS,
-        str(least_cases): CommandSubcategory.CORONA_SPREAD_LEAST_INFECTIONS,
-        str(least_deaths): CommandSubcategory.CORONA_SPREAD_LEAST_DEATHS,
-        str(least_recoveries): CommandSubcategory.CORONA_SPREAD_LEAST_RECOVERIES,
-        str(infections_by_query_1): CommandSubcategory.CORONA_INFECTIONS_BY_QUERY,
-        str(infections_by_query_2): CommandSubcategory.CORONA_INFECTIONS_BY_QUERY,
-        str(deaths_by_query): CommandSubcategory.CORONA_DEATHS_BY_QUERY,
-        str(recoveries_by_query): CommandSubcategory.CORONA_RECOVERIES_BY_QUERY,
-        str(new_cases_by_query): CommandSubcategory.CORONA_NEW_CASES_BY_QUERY
-    }
-
     def __init__(self, *args, **kwargs):
+        
+        data_timestamp_1 = {'när': ('uppdaterad', 'uppdaterades', 'statistik', 'statistiken')}
+        data_timestamp_2 = {'hur': ('gammal', 'data', 'datan')}
+
+        total_deaths = {'totalt': ('dött', 'omkommit', 'döda')}
+        total_recoveries = {'totalt': ('friska', 'tillfrisknat', 'återhämtat')}
+        total_cases = {'totalt': ('smittade', 'smittats', 'sjuka')}
+
+        most_deaths = {'flest': ('döda', 'dödsfall', 'omkommit', 'omkomna', 'dött')}
+        most_recoveries = {'flest': ('friska', 'tillfrisknat')}
+        most_cases = {'flest': ('smittade', 'sjuka')}
+
+        least_deaths = {'minst': ('döda', 'dödsfall', 'omkomna', 'omkommit', 'dött')}
+        least_recoveries = {'minst': ('smittade', 'sjuka')}
+        least_cases = {'minst': ('friska', 'tillfrisknat')}
+
+        infections_by_query_1 = {'har': ('smittats', 'sjuka')}
+        infections_by_query_2 = {'är': ('smittade', 'sjuka')}
+        deaths_by_query = {'har': ('dött', 'omkommit', 'döda')}
+        recoveries_by_query = {'har': ('friska', 'tillfrisknat')}
+        new_cases_by_query = {'hur': ('nya', 'nytt', 'fall')}    
+
         self.translation_file_path = kwargs['translation_file_path']
         self.command_parser = CoronaSpreadFeatureCommandParser(
-            category = CommandCategory.CORONA_SPREAD,
             keywords = CoronaSpreadFeature.FEATURE_KEYWORDS,
-            subcategories = CoronaSpreadFeature.FEATURE_SUBCATEGORIES
+            callbacks = {
+                str(data_timestamp_1): lambda: self.interface.get_data_timestamp(),
+                str(data_timestamp_2): lambda: self.interface.get_data_timestamp(),
+                str(total_deaths): lambda: self.get_total_deaths(),
+                str(total_recoveries): lambda: self.get_total_recoveries(),
+                str(total_cases): lambda: self.get_total_infections(),
+                str(most_deaths): lambda: self.get_most_deaths(),
+                str(most_recoveries): lambda: self.get_most_recoveries(),
+                str(most_cases): lambda: self.get_most_infections(),
+                str(least_cases): lambda: self.get_least_infections(),
+                str(least_deaths): lambda: self.get_least_deaths(),
+                str(least_recoveries): lambda: self.get_least_recoveries(),
+                str(infections_by_query_1): self.get_cases_by_country,
+                str(infections_by_query_2): self.get_cases_by_country,
+                str(deaths_by_query): self.get_deaths_by_country,
+                str(recoveries_by_query): self.get_recoveries_by_country,
+                str(new_cases_by_query): self.get_new_cases_by_country
+            }
         )
 
-        self.callbacks = {
-            CommandSubcategory.CORONA_SPREAD_TOTAL_DEATHS: lambda: self.get_total_deaths(),
-            CommandSubcategory.CORONA_SPREAD_TOTAL_RECOVERIES: lambda: self.get_total_recoveries(),
-            CommandSubcategory.CORONA_SPREAD_TOTAL_INFECTIONS: lambda: self.get_total_infections(),
-            CommandSubcategory.CORONA_SPREAD_MOST_DEATHS: lambda: self.get_most_deaths(),
-            CommandSubcategory.CORONA_SPREAD_MOST_RECOVERIES: lambda: self.get_most_recoveries(),
-            CommandSubcategory.CORONA_SPREAD_MOST_INFECTIONS: lambda: self.get_most_infections(),
-            CommandSubcategory.CORONA_SPREAD_LEAST_INFECTIONS: lambda: self.get_least_infections(),
-            CommandSubcategory.CORONA_SPREAD_LEAST_DEATHS: lambda: self.get_least_deaths(),
-            CommandSubcategory.CORONA_SPREAD_LEAST_RECOVERIES: lambda: self.get_least_recoveries(),
-            CommandSubcategory.CORONA_DATA_TIMESTAMP: lambda: self.interface.get_data_timestamp(),
-            CommandSubcategory.CORONA_INFECTIONS_BY_QUERY: self.get_cases_by_country,
-            CommandSubcategory.CORONA_DEATHS_BY_QUERY: self.get_deaths_by_country,
-            CommandSubcategory.CORONA_RECOVERIES_BY_QUERY: self.get_recoveries_by_country,
-            CommandSubcategory.CORONA_NEW_CASES_BY_QUERY: self.get_new_cases_by_country
-        }
 
         self.interactive_methods = (
             self.get_cases_by_country,
@@ -97,13 +79,12 @@ class CoronaSpreadFeature(fw.FeatureBase):
             CommandPronoun.INTERROGATIVE,
         )
 
-        api_handle = coronafeatureclient.ApiHandle(uri = kwargs['CORONA_API_URI'], standby_hours = 1)
+        api_handle = coronafeatureclient.ApiHandle(uri = kwargs['CORONA_API_URI'], standby_hours = 0.1)
         api_handle.add_header('x-rapidapi-host', kwargs['CORONA_API_RAPIDAPI_HOST'])
         api_handle.add_header('x-rapidapi-key', kwargs['CORONA_API_RAPIDAPI_KEY'])
 
         super().__init__(
             command_parser = self.command_parser,
-            callbacks = self.callbacks,
             interface = coronafeatureclient.Client(api_handle, self.translation_file_path),
             interactive_methods = self.interactive_methods
         )
